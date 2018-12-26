6bc709079f0c04f595742c1e35bf99ee9f0d42fe
David Anderson <david.anderson@intellinet.com>
2018-12-26 16:59:38 -0500

    Added sqlprovider. But it's still very slow

--- build/app/intermediates/flutter/release/frontend_server.d
+++
@@ -1,1 +0,0 @@
-frontend_server.d: C:\Dev\flutter\flutterInstall\flutter\bin\cache\artifacts\engine\windows-x64\frontend_server.dart.snapshot
--- build/app/intermediates/flutter/release/gen_snapshot.d
+++
@@ -1,1 +0,0 @@
-gen_snapshot.d: C:\Dev\flutter\flutterInstall\flutter\bin\cache\artifacts\engine\android-arm-release\windows-x64\gen_snapshot
